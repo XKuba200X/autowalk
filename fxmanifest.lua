e8e6e3a5bd43e254fa6fb8e1242f0b179222333f
Kuba200 <76246649+XKuba200X@users.noreply.github.com>
2023-08-10 14:47:55 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,13 @@ fx_version 'adamant'
 
 game'gta5'
 
-description'Skrypt na automatyczne chodzenie'
+autor 'Kuba200'
+description'Skrypt na automatyczne chodzenie z wyborem animacji'
 
-version'1.1'
+version'1.2'
 
 client_scripts {
   'move.lua'
 }
+
+hared_script '@es_extended/imports.lua'
